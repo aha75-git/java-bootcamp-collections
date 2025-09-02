5aecda4471b73541a203e218f696a6257a8e82eb
lazio <ahaffner75@gmail.com>
2025-09-02 15:54:19 +0200

    Aufgabe implementiert: Schulobjekt anstelle einer ArrayList eine Hashmap verwendet.

--- src/main/java/org/example/bootcamp/collections/Schule.java
+++ src/main/java/org/example/bootcamp/collections/Schule.java
@@ -1,29 +1,39 @@
 package org.example.bootcamp.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Schule {
     private static int idCount = 1;
     private final List<Student> students;
+    private final Map<String, Student> studentMap;
 
     public Schule() {
         students = new ArrayList<>();
+        studentMap = new HashMap<>();
     }
 
     public List<Student> getStudents() {
         return students;
     }
 
-    public boolean addStudent(Student student) {
-        return this.students.add(student);
+    public Map<String, Student> getStudentMap() {
+        return studentMap;
     }
 
-    public boolean removeStudent(Student student) {
-        return this.students.remove(student);
+    public void addStudent(Student student) {
+        this.studentMap.put(student.getStudentId(), student);
+        this.students.add(student);
     }
 
-    public Student getStudentById(String id) {
+    public void removeStudent(Student student) {
+        this.studentMap.remove(student.getStudentId());
+        this.students.remove(student);
+    }
+
+    public Student getStudentByIdFromList(String id) {
         for (Student student : students) {
             if(student.getStudentId().equals(id)) {
                 return student;
@@ -32,11 +42,28 @@ public class Schule {
         return null;
     }
 
+    public Student getStudentById(String id) {
+        for(Map.Entry<String, Student> entry : studentMap.entrySet()) {
+            if(entry.getValue().getStudentId().equals(id)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
     public void printStudents() {
+        for (Map.Entry<String, Student> entry : studentMap.entrySet()) {
+            Student student = entry.getValue();
+            System.out.println(student.getFirstName() + " " + student.getLastName() + " (ID: " + student.getStudentId() + ")");
+        }
+    }
+
+    public void printStudentsFromList() {
         for (Student student : students) {
             System.out.println(student.getFirstName() + " " + student.getLastName() + " (ID: " + student.getStudentId() + ")");
         }
     }
+
 
     public List<Kurs> getKurseByStudentId(String studentID) {
         Student student = getStudentById(studentID);
