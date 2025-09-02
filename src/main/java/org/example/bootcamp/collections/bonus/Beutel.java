a9f195c6c149aff67361df0fd12167735b263057
lazio <ahaffner75@gmail.com>
2025-09-02 15:35:36 +0200

    Aufgabe: Beutel-Klasse wie LinkedList implementiert.

---
+++ src/main/java/org/example/bootcamp/collections/bonus/Beutel.java
@@ -0,0 +1,84 @@
+package org.example.bootcamp.collections.bonus;
+
+public class Beutel {
+    private int size = 0;
+    private Node head;
+
+    //Implementiere Schritt für Schritt die Methoden add, get, size, remove last, remove by index und
+
+    public void add(int element) {
+        Node newNode = new Node(element);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node currentNode = this.head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
+        }
+        Node currentNode = this.head;
+        for(int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.element;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= this.size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + this.size);
+        }
+
+        if (index == 0) {
+            head = head.next;
+        }  else {
+            Node currentNode = this.head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = currentNode.next.next;
+        }
+        size--;
+    }
+
+    public void removeLast() {
+        if(head == null || head.next == null) {
+            head = null;
+            return;
+        }
+        Node currentNode = this.head;
+        for (int i = 0; i < this.size - 1; i++) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = null;
+        size--;
+    }
+
+    class Node {
+        int element;
+        Node next;
+        Node(int element) {
+            this.element = element;
+        }
+    }
+
+    public void printElements() {
+        Node currentNode = this.head;
+        for(int i = 0; i < this.size; i++) {
+            System.out.print(currentNode.element + " ");
+            currentNode = currentNode.next;
+        }
+    }
+}
---
+++ src/main/java/org/example/bootcamp/collections/bonus/BeutelMain.java
@@ -0,0 +1,48 @@
+package org.example.bootcamp.collections.bonus;
+
+public class BeutelMain {
+    public static void main(String[] args) {
+
+        Beutel beutel = new Beutel();
+        beutel.add(1);
+        beutel.add(2);
+        beutel.add(3);
+        beutel.add(4);
+        beutel.add(15);
+        beutel.add(16);
+        beutel.add(33);
+
+        beutel.printElements();
+        System.out.println();
+
+        System.out.println("Size: " + beutel.size());
+        System.out.println("Element am index 1: " + beutel.get(1));
+        System.out.println();
+
+        System.out.println("Das letzte Element : " + beutel.get(beutel.size() - 1));
+        beutel.removeLast();
+        System.out.println("Size nach dem Löschen des letzten Elementes: " + beutel.size());
+        System.out.println("Das letzte Element : " + beutel.get(beutel.size() - 1));
+        System.out.println();
+
+        System.out.println("Das Element am index 3: " + beutel.get(3)); // 4
+        beutel.remove(3);
+        System.out.println("Size nach dem Löschen am index 3: " + beutel.size());
+        System.out.println("Das Element am index 3: " + beutel.get(3)); // 15
+        System.out.println();
+
+        beutel.printElements();
+        System.out.println();
+
+        System.out.println("Element am index 2: " + beutel.get(2));
+        beutel.add(66);
+        System.out.println("Size: " + beutel.size());
+        System.out.println();
+        beutel.add(55);
+        System.out.println("Size: " + beutel.size());
+        System.out.println("Element am index 6: " + beutel.get(6));
+
+        beutel.printElements();
+        System.out.println();
+    }
+}
